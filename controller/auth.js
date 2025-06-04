f4b2d8042209804a50a1c330cc1b4d0f68fc3e03
apple <apple@apples-MacBook-Pro.local>
2025-06-04 20:20:56 +0530

    prod ready

--- controller/auth.js
+++ controller/auth.js
@@ -4,8 +4,8 @@ import pool from '../config/db.js';
 
 
 
-export const register = async(req, res) => {
-    const { email, password } = req.body;
+export const register = async (req, res) => {
+  const { email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   try {
     await pool.query('INSERT INTO admins (email, password) VALUES (?, ?)', [email, hashedPassword]);
@@ -16,23 +16,22 @@ export const register = async(req, res) => {
   }
 }
 
-export const login = async(req, res) => {
-    const { email, password } = req.body;
-    const [rows] = await pool.query('SELECT * FROM admins WHERE email = ?', [email]);
-    if (rows.length === 0) return res.status(401).json({ message: 'Invalid credentials' });
-    const admin = rows[0];
-    const isMatch = await bcrypt.compare(password, admin.password);
-    if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
-  
-    const token = jwt.sign({ email: admin.email }, process.env.JWT_SECRET);
-    res.cookie('token', token, { httpOnly: true, sameSite: 'Strict' });
-    res.json({ message: 'Login successful', email, token });
-    res.sendStatus(200);
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  const [rows] = await pool.query('SELECT * FROM admins WHERE email = ?', [email]);
+  if (rows.length === 0) return res.status(401).json({ message: 'Invalid credentials' });
+  const admin = rows[0];
+  const isMatch = await bcrypt.compare(password, admin.password);
+  if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+
+  const token = jwt.sign({ email: admin.email }, process.env.JWT_SECRET);
+  res.cookie('token', token, { httpOnly: true, sameSite: 'Strict' });
+  return res.status(200).json({ message: 'Login successful', email, token });
 }
 
 export const logout = (req, res) => {
-    res.clearCookie('token');
-    res.sendStatus(200);
+  res.clearCookie('token');
+  res.sendStatus(200);
 }
 
 export const checkAuth = (req, res) => {
